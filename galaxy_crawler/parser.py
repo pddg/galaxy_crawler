37cbcdd4580e96513adf0a37f325aa3ccfad8d3b
Pudding <s.kokuryo@gmail.com>
2019-06-17 00:57:54 +0900

    Update crawler and parser

--- galaxy_crawler/parser.py
+++ galaxy_crawler/parser.py
@@ -3,10 +3,13 @@ from queue import Queue, Empty
 from logging import getLogger
 from typing import TYPE_CHECKING
 
+from galaxy_crawler.crawl import Response, Request
+from galaxy_crawler.constants import Target
+
 if TYPE_CHECKING:
-    from typing import List
+    from typing import List, Dict, Optional
     from galaxy_crawler.filters import Filter
-    from galaxy_crawler.queries import QueryBuilder
+    from galaxy_crawler.queries import QueryBuilder, QueryOrder
     from galaxy_crawler.repositories import ResponseDataStore
 
 logger = getLogger(__name__)
@@ -15,66 +18,93 @@ logger = getLogger(__name__)
 class ResponseParser(Thread):
 
     def __init__(self, url_queue: 'Queue', json_queue: 'Queue', data_stores: 'List[ResponseDataStore]',
-                 query_builder: 'QueryBuilder', filters: 'List[Filter]'):
+                 query_builder: 'QueryBuilder', order: 'QueryOrder', targets: 'List[Target]', filters: 'List[Filter]'):
         super(ResponseParser, self).__init__()
         self.url_q = url_queue
         self.json_q = json_queue
         self.data_stores = data_stores
         self.query_builder = query_builder
         self.filters = filters
+        self.targets = targets
+        self.order = order
         self._stop_signal = False
+        self._save_trial = 0
 
     def run(self) -> None:
-        initial_url = self.query_builder.search().build()
-        self.url_q.put(initial_url)
-        while not self._stop_signal:
-            try:
-                json_obj = self.json_q.get(timeout=3)
-            except Empty:
-                continue
-            if json_obj is None:
-                logger.info("Scraping finished")
-                self.json_q.task_done()
-                break
-            if 'next_link' in json_obj:
-                self.push_next_link(json_obj['next_link'])
-            if 'results' not in json_obj:
-                logger.critical("Failed to parse response. Returned json has no results.")
-                break
-            results = json_obj['results']
-            logger.info(f"{len(results)} repositories were found.")
-            for repository in results:
-                # Show debug information
-                self.show_summary(repository)
-                # Filtering objects
-                if not all([f.passed(repository) for f in self.filters]):
+        try:
+            for target in self.targets:
+                if self._stop_signal:
+                    break
+                initial_url = self.get_url_from(target)
+                self.push_next_link(target, initial_url)
+                while not self._stop_signal:
+                    try:
+                        response = self.json_q.get(timeout=3)  # type: Optional[Response]
+                    except Empty:
+                        logger.debug("Wait for next response...")
+                        continue
+                    json_obj = response.response
+                    if json_obj is None:
+                        logger.info("Scraping finished")
+                        self.json_q.task_done()
+                        self.send_stop_signal()
+                        break
+                    results = json_obj.get('results')
+                    if results is None:
+                        logger.critical("Failed to parse response. Returned json has no results.")
+                        self.send_stop_signal()
+                        break
+                    self.add_items(response.target, results)
+                    self.save()
+                    self.json_q.task_done()
+                    next_link = json_obj.get('next_link')
+                    if next_link is not None:
+                        self.push_next_link(response.target, next_link)
+                    else:
+                        # Finish to obtain this kind of items
+                        break
+        finally:
+            self.save()
+            logger.info("Parser finished")
+
+    def make_request(self, target: 'Target', url: str) -> Request:
+        return Request(target, url)
+
+    def get_url_from(self, target: 'Target'):
+        if target in [Target.REPOSITORIES, Target.ROLES]:
+            url = self.query_builder \
+                .order_by(self.order) \
+                .build(target)
+        else:
+            url = self.query_builder \
+                .build(target)
+        return url
+
+    def add_items(self, target: 'Target', items: 'dict'):
+        logger.info(f"{len(items)} items were found.")
+        to_save = []
+        for item in items:
+            # Filtering objects
+            if len(self.filters) > 0:
+                if not all([f.passed(target, item) for f in self.filters]):
                     self.send_stop_signal()
                     break
-                for store in self.data_stores:
-                    store.save(repository)
-            self.json_q.task_done()
-            if not self._stop_signal:
-                logger.info("Wait for next response...")
-        self.finish()
-
-    def show_summary(self, repository: dict):
-        try:
-            namespace = repository['summary_fields']['provider_namespace']['name']
-            logger.debug(f"name: '{namespace}.{repository['name']}', description: '{repository['description']}'")
-        except AttributeError:
-            logger.warning("This repository has no namespace.")
-
-    def finish(self):
-        logger.info("Saving obtained repositories information...")
+            to_save.append(item)
         for store in self.data_stores:
-            store.commit()
-        logger.info("Parser finished")
+            store.save(target, to_save)
+
+    def save(self):
+        self._save_trial += 1
+        if self._save_trial % 5 == 0:
+            logger.info("Saving obtained items information...")
+            for store in self.data_stores:
+                store.commit()
 
     def send_stop_signal(self):
         self._stop_signal = True
 
-    def push_next_link(self, link):
+    def push_next_link(self, target: 'Target', link):
         next_link = self.query_builder.join(link)
         logger.info(f"Next link was found: {next_link}")
-        self.url_q.put(next_link)
+        self.url_q.put(self.make_request(target, next_link))
 
