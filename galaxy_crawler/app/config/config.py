6eb493f97050b92ecabee1cf4e8ab99c4701cd6c
Pudding <s.kokuryo@gmail.com>
2019-06-17 00:54:18 +0900

    Support targets in config

--- galaxy_crawler/app/config/config.py
+++ galaxy_crawler/app/config/config.py
@@ -1,8 +1,13 @@
 import distutils
+import argparse
 import os
 import warnings
 from pathlib import Path
 from typing import TYPE_CHECKING
+
+from galaxy_crawler.queries.v1 import V1QueryOrder
+from galaxy_crawler.filters.v1 import V1FilterEnum
+from galaxy_crawler.constants import Target
 
 if TYPE_CHECKING:
     from typing import Union, List, Optional
@@ -50,8 +55,7 @@ class Config(object):
     DEFAULT_FILTERS = []
 
     def __init__(self, output_dir: 'Path', version: str, interval: int, retry: int, output_format: 'List[str]',
-                 debug: 'bool', order_by: str, inverse: bool, filters: 'List[str]', log_dir: 'Path' = None,
-                 **kwargs):
+                 debug: 'bool', order_by: str, inverse: bool, filters: 'List[str]', log_dir: 'Path' = None, **kwargs):
         assert interval >= 0, "Interval must be a positive value."
         assert retry >= 0, "Retry must be a positive value."
         self.output_dir = output_dir
@@ -64,6 +68,15 @@ class Config(object):
         self.order_by = order_by
         self.inverse = inverse
         self.filters = filters
+        # TODO: To support to select targets by option
+        self.targets = [
+            Target.TAGS,
+            Target.PLATFORMS,
+            Target.PROVIDERS,
+            Target.NAMESPACES,
+            Target.PROVIDER_NAMESPACES,
+            Target.ROLES
+        ]
 
     @classmethod
     def load(cls, args: 'Union[Namespace, dict]') -> 'Config':
@@ -118,3 +131,31 @@ class Config(object):
             config_key = "DEFAULT_" + key.upper()
             config_dict[key] = getattr(cls, config_key, None)
         return config_dict
+
+    @classmethod
+    def get_parser(cls) -> 'argparse.ArgumentParser':
+        parser = argparse.ArgumentParser(description="Ansible Galaxy crawler")
+
+        log_parser = argparse.ArgumentParser(add_help=False)
+        log_group = log_parser.add_argument_group("LOGGING")
+        log_group.add_argument("--debug", action="store_true", default=False, help="Enable debug logging")
+        log_group.add_argument("--log-dir", type=Path, help="Log output directory")
+
+        cmd_parser = parser.add_subparsers(dest="func")
+        start_cmd = cmd_parser.add_parser("start", help="Start crawling", parents=[log_parser])
+        start_cmd.add_argument("output_dir", type=Path, help="Path to output")
+        start_cmd.add_argument("--version", choices=["v1"], help="The API version of galaxy.ansible.com")
+        start_cmd.add_argument("--interval", type=int,
+                               help=f"Fetch interval (default={cls.DEFAULT_INTERVAL})")
+        start_cmd.add_argument("--retry", type=int,
+                               help=f"Number of retrying (default={cls.DEFAULT_RETRY})")
+        start_cmd.add_argument("--format", choices=["json"], nargs="+", dest='output_format',
+                               help=f"Output format (default={cls.DEFAULT_OUTPUT_FORMAT})")
+        start_cmd.add_argument("--order-by", choices=V1QueryOrder.choices(),
+                               help=f"Query order (default={cls.DEFAULT_ORDER_BY}). It is a descending order by default.")
+        start_cmd.add_argument("--inverse", action="store_true",
+                               help="If this specified, make the order of query inverse")
+        start_cmd.add_argument("--filters", type=str, nargs='*',
+                               help=f"Filter expression (e.g. download>500). "
+                               f"Available filter types are {V1FilterEnum.choices()}")
+        return parser
--- galaxy_crawler/cli.py
+++ galaxy_crawler/cli.py
@@ -1,29 +1,20 @@
-import argparse
 import logging
-from pathlib import Path
+from time import sleep
 
 from galaxy_crawler.app.config import Config
 from galaxy_crawler.app.di import AppComponent
-from galaxy_crawler.queries.v1 import V1QueryOrder
-from galaxy_crawler.filters.v1 import V1FilterEnum
 from galaxy_crawler.errors import NotSupportedFilterError, InvalidExpressionError
-from time import sleep
-
-DEFAULT_INTERVAL = Config.DEFAULT_INTERVAL
-DEFAULT_RETRY_COUNT = Config.DEFAULT_RETRY
 
 logger = logging.getLogger(__name__)
 
 
-def scrape(appconfig: 'Config'):
-    components = AppComponent(appconfig)
-    components.configure_logger()
+def scrape(components: 'AppComponent'):
     try:
         crawler = components.get_crawler()
         parser = components.get_parser()
     except (NotSupportedFilterError, InvalidExpressionError) as e:
         logger.error(e)
-        exit(1)
+        return 1
 
     # Start to crawl and parse on another thread
     crawler.start()
@@ -43,42 +34,23 @@ def scrape(appconfig: 'Config'):
         if crawler.is_alive():
             crawler.send_stop_signal()
             crawler.join()
+    return 0
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Ansible Galaxy crawler")
-
-    log_parser = argparse.ArgumentParser(add_help=False)
-    log_group = log_parser.add_argument_group("LOGGING")
-    log_group.add_argument("--debug", action="store_true", default=False, help="Enable debug logging")
-    log_group.add_argument("--log-dir", type=Path, help="Log output directory")
-
-    cmd_parser = parser.add_subparsers()
-    start_cmd = cmd_parser.add_parser("start", help="Start crawling", parents=[log_parser])
-    start_cmd.add_argument("output_dir", type=Path, help="Path to output")
-    start_cmd.add_argument("--version", choices=["v1"], help="The API version of galaxy.ansible.com")
-    start_cmd.add_argument("--interval", type=int,
-                           help=f"Fetch interval (default={DEFAULT_INTERVAL})")
-    start_cmd.add_argument("--retry", type=int,
-                           help=f"Number of retrying (default={DEFAULT_RETRY_COUNT})")
-    start_cmd.add_argument("--format", choices=["json"], nargs="+", dest='output_format',
-                           help=f"Output format (default={Config.DEFAULT_OUTPUT_FORMAT})")
-    start_cmd.add_argument("--order-by", choices=V1QueryOrder.choices(),
-                           help=f"Query order (default={Config.DEFAULT_ORDER_BY}). It is a descending order by default.")
-    start_cmd.add_argument("--inverse", action="store_true",
-                           help="If this specified, make the order of query inverse")
-    start_cmd.add_argument("--filters", type=str, nargs='*',
-                           help=f"Filter expression (e.g. download>500). "
-                           f"Available filter types are {V1FilterEnum.choices()}")
-    start_cmd.set_defaults(func=scrape)
+    parser = Config.get_parser()
     args = parser.parse_args()
     try:
         config = Config.load(args)
-        args.func(config)
+        components = AppComponent(config)
+        components.configure_logger()
     except (ValueError, NotADirectoryError) as e:
         print(e)
-        exit(1)
+        return 1
+    if args.func == 'start':
+        return scrape(components)
 
 
 if __name__ == '__main__':
-    main()
+    exit_code = main()
+    exit(exit_code)
