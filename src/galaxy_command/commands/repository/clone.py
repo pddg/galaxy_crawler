9ac3a729ed457ed8cdfb6539e91bb70eb404786d
Pudding <s.kokuryo@gmail.com>
2019-11-07 15:16:52 +0900

    Support dynamic date range filtering when cloning roles

--- src/galaxy_command/commands/repository/clone.py
+++ src/galaxy_command/commands/repository/clone.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -7,18 +7,25 @@ import pandas as pd
 import uroboros
 from uroboros.constants import ExitStatus
 
+from galaxy_command.commands.database.options import StorageOption
 from galaxy_crawler.ghq import GHQ
 from galaxy_crawler.models import v1 as models
 from galaxy_crawler.models.utils import get_scoped_session
 
-from galaxy_command.commands.database.options import StorageOption
-
 if TYPE_CHECKING:
     import argparse
-    from typing import Union
-    from galaxy_crawler.app.di import AppComponent
+    from typing import Union, List, Optional
+    from galaxy_command.app.di import AppComponent
 
 logger = logging.getLogger(__name__)
+
+date_format = "%Y/%m/%d"
+
+
+def _to_date(date_str: 'Optional[str]') -> 'Optional[datetime]':
+    if date_str is not None:
+        return datetime.strptime(date_str, date_format)
+    return date_str
 
 
 def _get_roles_with_repos(engine) -> 'pd.DataFrame':
@@ -38,12 +45,15 @@ def _get_roles_with_repos(engine) -> 'pd.DataFrame':
 
 def _get_roles_limited(roles: 'pd.DataFrame',
                        threshold: float,
-                       last_updated_in: int = 365) -> 'pd.DataFrame':
-    obtained_at = datetime(2019, 10, 22)
-    year_ago = obtained_at - timedelta(days=last_updated_in)
-    logger.info(f"Filtering roles updated between {str(obtained_at)} and {str(year_ago)}")
-    recently_roles = roles.loc[obtained_at >= roles["modified"]]
-    recently_roles = recently_roles.loc[roles["modified"] >= year_ago]
+                       from_date: 'Optional[datetime]',
+                       to_date: 'Optional[datetime]') -> 'pd.DataFrame':
+    if from_date is None:
+        from_date = roles["modified"].min()
+    if to_date is None:
+        to_date = roles["modified"].max()
+    logger.info(f"Filtering roles updated between {str(from_date)} and {str(to_date)}")
+    masks = (roles["modified"] <= to_date) & (roles["modified"] >= from_date)
+    recently_roles = roles.loc[masks]
     logger.info(f"{len(recently_roles)} roles were found")
     logger.info(f"Filtering roles by {threshold * 100} percentile")
     threshold = _get_threshold(roles, threshold)
@@ -71,11 +81,49 @@ class CloneCommand(uroboros.Command):
     long_description = 'Clone repositories from GitHub based on given DB'
     options = [StorageOption()]
 
+    def validate(self, args: 'argparse.Namespace') -> 'List[Exception]':
+        errors = []
+        p = args.percentile
+        if not 0 < p < 1:
+            errors.append(Exception(f"'percentile' must be grater than 0 and smaller than 1 ({p} is given)"))
+        try:
+            _to_date(args.date_from)
+            _to_date(args.date_to)
+        except Exception as e:
+            errors.append(e)
+        return errors
+
+    def after_validate(self, safe_args: 'argparse.Namespace') -> 'argparse.Namespace':
+        safe_args.date_from = _to_date(safe_args.date_from)
+        safe_args.date_to = _to_date(safe_args.date_to)
+        return safe_args
+
     def build_option(self, parser: 'argparse.ArgumentParser') -> 'argparse.ArgumentParser':
-        parser.add_argument("output_dir", type=Path, help="Path to clone")
-        parser.add_argument("--interval", type=int, default=5, help="Clone interval")
-        parser.add_argument("--percentile", type=float, default=0.9, help="Threshold to clone")
-        parser.add_argument("--n-jobs", type=int, default=6, help="Number of parallel jobs")
+        parser.add_argument("output_dir",
+                            type=Path,
+                            help="Path to clone")
+        parser.add_argument("--interval",
+                            type=int,
+                            default=5,
+                            help="Clone interval")
+        parser.add_argument("--percentile",
+                            type=float,
+                            default=0.9,
+                            help="Threshold to clone")
+        parser.add_argument("--n-jobs",
+                            type=int,
+                            default=6,
+                            help="Number of parallel jobs")
+        parser.add_argument("--date-from",
+                            type=str,
+                            help="Lower limit of modified date of role (YYYY/MM/DD)")
+        parser.add_argument("--date-to",
+                            type=str,
+                            help="Upper limit of modified date of role (YYYY/MM/DD)")
+        parser.add_argument("--dry-run",
+                            action='store_true',
+                            default=False,
+                            help="Show the number of repository to clone (do not clone)")
         return parser
 
     def run(self, args: 'argparse.Namespace') -> 'Union[ExitStatus, int]':
@@ -87,9 +135,10 @@ class CloneCommand(uroboros.Command):
         try:
             engine = components.get_engine()
             roles = _get_roles_with_repos(engine)
-            roles = _get_roles_limited(roles, args.percentile)
+            roles = _get_roles_limited(roles, args.percentile, args.date_from, args.date_to)
             repositories = _get_repository_urls(roles)
-            ghq.clone(repositories)
+            if not args.dry_run:
+                ghq.clone(repositories)
         except KeyboardInterrupt:
             return ExitStatus.FAILURE
         return ExitStatus.SUCCESS
